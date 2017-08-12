39197c8f09f37b96aee84065d0eec69fa36386e4
Jimmy McWhinney <jimmy.mcw@live.com>
2017-08-12 13:58:50 +0800

    Add project files.

---
+++ PayPal/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PayPal.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
